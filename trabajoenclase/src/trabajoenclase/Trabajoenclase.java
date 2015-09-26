f5b38d5d09fc4f3623dfdf9f93b296ff29645a3e
coreduacion <coreduacion@coreduacion9>
2015-09-26 11:22:33 -0500

    archivo de interfaz

--- trabajoenclase/src/trabajoenclase/Trabajoenclase.java
+++ trabajoenclase/src/trabajoenclase/Trabajoenclase.java
@@ -15,7 +15,7 @@ public class Trabajoenclase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       suma s - new suma [];
+       suma s = new suma ();
        s.setLocationRelativeTo (null);
        s.setVisible (true);
     }
